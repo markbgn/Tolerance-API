9bb78d3c421692d3f7b180bd5cdc4de721efee52
Mark Bogan <boganmark63@gmail.com>
2023-02-07 19:58:48 +0100

    First request, test data.

--- Tolerance_API_app/app.js
+++ Tolerance_API_app/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server accessed");
+});
 
 module.exports = app;
